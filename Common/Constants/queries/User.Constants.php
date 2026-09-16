5d7202cd361d1280fa93f2147bcbebd1f3cb1a0f
gb21oc <luckygb21@gmail.com>
2026-09-16 09:21:52 -0300

    feat: criado nova constante para buscar o id do aluno

--- Common/Constants/queries/User.Constants.php
+++ Common/Constants/queries/User.Constants.php
@@ -4,6 +4,7 @@ namespace Common\Constants\queries;
 
 class UserConstants
 {
+    public const GET_BY_STUDENT_ID = "SELECT * FROM alunos WHERE id = ?";
     public const UPDATE_LAST_LOGIN = "UPDATE admin_usuarios SET ultimo_login = NOW() WHERE id = ?";
     public const FIND_BY_USER = "SELECT id, nome, usuario, senha_hash, nivel, ativo FROM admin_usuarios WHERE usuario = ? LIMIT 1";
 }
